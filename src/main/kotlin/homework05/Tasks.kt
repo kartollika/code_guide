1fa4e5dd85bb0eabdd2510d57b30bce7858b3b51
kartollika <maksimow.dmitrij@yandex.ru>
2018-05-02 21:55:04 +0300

    Created function to get listOfNearests and matrixOfNearests Implemented floydWarshall

--- src/main/kotlin/homework05/Tasks.kt
+++ src/main/kotlin/homework05/Tasks.kt
@@ -6,4 +6,36 @@ fun evenTrees(n: Int, edges: List<Pair<Int, Int>>): Int = -1
 
 fun dijkstra(n: Int, edges: List<Triple<Int, Int, Int>>, startNode: Int): List<Int> = emptyList()
 
-fun floydWarshall(n: Int, edges: List<Triple<Int, Int, Int>>): List<List<Int>> = emptyList()
+fun floydWarshall(n: Int, edges: List<Triple<Int, Int, Int>>): List<List<Int>> {
+    val dist = getMatrixOfNearests(n, edges)
+    for (k in 0 until n) {
+        for (i in 0 until n) {
+            for (j in 0 until n) {
+                dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j])
+            }
+        }
+    }
+    return dist.toList()
+}
+
+fun getListOfNearests(n: Int, edges: List<Triple<Int, Int, Int>>): List<ArrayList<Pair<Int, Int>>> {
+    val listOfNearests: List<ArrayList<Pair<Int, Int>>> = List(n, { ArrayList<Pair<Int, Int>>() })
+
+    for (i in 0 until edges.size) {
+        listOfNearests[edges[i].first].add(Pair(edges[i].second, edges[i].third))
+    }
+    return listOfNearests
+}
+
+fun getMatrixOfNearests(n: Int, edges: List<Triple<Int, Int, Int>>): Array<MutableList<Int>> {
+    val matrix = Array(n, { MutableList(n, { Int.MIN_VALUE }) })
+
+    for (i in 0 until n) {
+        matrix[i][i] = 0
+    }
+
+    for (edge: Triple<Int, Int, Int> in edges) {
+        matrix[edge.first][edge.second] = edge.third
+    }
+    return matrix
+}
